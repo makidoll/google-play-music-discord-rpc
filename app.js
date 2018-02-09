116d70e898ccde8f80b01b58303b9bde267f3680
Maxine Deprez <mxmcube@gmail.com>
2018-02-09 17:40:51 +0000

    Updates

--- app.js
+++ app.js
@@ -1,44 +1,71 @@
-const express = require("express");
-const bodyParser = require("body-parser")
-const { Client } = require("discord-rpc");
-const keys = require(__dirname+"/config.js");
-var app = express();
+//const express = require("express");
+//var bodyParser = require("body-parser")
+var { Client } = require("discord-rpc");
+var keys = require(__dirname+"/config.js");
+var server = require("http").createServer();
+var io = require("socket.io")(server);
+var moment = require("moment");
+//var app = express();
 
 const port = 13370;
 const rpc = new Client({ transport: "ipc" });
 
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
 
-function decode(input) { // oh yeah its trash
-	let str = input.match(/.{1,2}/g);
-	let out = "";
-	for (let i=0; i<str.length; i++) {
-		out += String.fromCharCode(parseInt(str[i], 36));
-	}
-	return out;
-}
+// function decode(input) { // oh yeah its trash
+// 	let str = input.match(/.{1,2}/g);
+// 	let out = "";
+// 	for (let i=0; i<str.length; i++) {
+// 		out += String.fromCharCode(parseInt(str[i], 36));
+// 	}
+// 	return out;
+// }
 
-app.post("/google-play-music-rpc", function(req, res) {
-	try {
+// app.post("/google-play-music-rpc", function(req, res) {});
 
-		let now = JSON.parse(decode(Object.keys(req.body)[0]));
-		console.log(now.artist+" - "+now.title+" ("+now.album+")");
+var current_song = {}
 
-		rpc.setActivity({
-			details: "🎵  "+now.title,
-			state: "👤  "+now.artist,
-			largeImageKey: keys.imageKeys.large,
-			smallImageKey: keys.imageKeys.small,
-			largeImageText: "💿  "+now.album,
-			//smallImageText: "💿  "+now.album,
-			instance: false,
-		});
+io.on("connection", function(socket){
+	console.log(socket.handshake.address.split(":")[3]+" connected!");
 
-	} catch(err) { console.log(err); } ;
+	socket.on("song_change", function(now) {
+		//let current = now.current;
+		//delete now.current;
+
+		let json = JSON.stringify(now);
+		if (current_song != json) {
+			current_song = json;
+			//now.current = current;
+			try {
+				console.log(now.artist+" - "+now.title+" ("+now.album+") ("+now.duration+")");
+
+				let end = moment()
+					.add(now.duration.split(":")[0], "m")
+					.add(now.duration.split(":")[1], "s")
+					.unix();
+
+				rpc.setActivity({
+					details: "🎵  "+now.title,
+					state: "👤  "+now.artist,
+					startTimestamp: moment().unix(),
+					endTimestamp: end,
+					largeImageKey: keys.imageKeys.large,
+					smallImageKey: keys.imageKeys.small,
+					largeImageText: "💿  "+now.album,
+					//smallImageText: "💿  "+now.album,
+					instance: false,
+				});
+			} catch(err) { console.log(err); } ;
+		}
+	});
+
+	socket.on("disconnect", function() {
+		console.log(socket.handshake.address.split(":")[3]+" disconnected!");
+	});
 });
 
-app.listen(port, function() {
-	console.log("Server open at 127.0.0.1:"+port);
+server.listen(port, function() {
+	console.log("Socket open at 127.0.0.1:"+port);
 });
 
 rpc.on("ready", function() {
--- google-play-music-rpc.user.js
+++ google-play-music-rpc.user.js
@@ -4,24 +4,45 @@
 // @grant none
 // @author Maki
 // @match *://play.google.com/*
+// @require https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js
+// @icon https://github.com/makixx/google-play-music-discord-rpc/blob/master/images/rpc-cover.png?raw=true
 // ==/UserScript==
 
-function myStupidEncodingThing(str) {
-	let out = "";
-	for (let i=0; i<str.length; i++) {
-		out += ("00"+str.charCodeAt(i).toString(36)).slice(-2);
-	}
-	return out;
-}
+// function myStupidEncodingThing(str) {
+// 	let out = "";
+// 	for (let i=0; i<str.length; i++) {
+// 		out += ("00"+str.charCodeAt(i).toString(36)).slice(-2);
+// 	}
+// 	return out;
+// }
+window.maki = {};
+window.maki.interval = null;
+window.maki.socket = io("http://127.0.0.1:13370");
 
-setInterval(function() {
+window.maki.socket.on("connect", function() {
+	window.maki.interval = window.setInterval(function() {
+		try {	
+			window.maki.socket.emit("song_change", {
+				title: document.getElementById("currently-playing-title").textContent,
+				artist: document.getElementById("player-artist").textContent,
+				album: document.querySelector(".player-album").textContent,
+				duration: document.getElementById("time_container_duration").textContent,
+				//current: document.getElementById("time_container_current").textContent
+			});
 
-	let update = JSON.stringify({
-		title: document.querySelector(".now-playing-info-content #currently-playing-title").textContent,
-		artist: document.querySelector(".now-playing-info-content .player-artist").textContent,
-		album: document.querySelector(".now-playing-info-content .player-album").textContent
-	})
+			// let xhr = new XMLHttpRequest();
+			// xhr.open("POST", "http://127.0.0.1:13370/google-play-music-rpc", true);
+			// xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			// xhr.send(myStupidEncodingThing(update));
+		} catch(err) {}
+	}, 1000);
+});
 
+<<<<<<< HEAD
+window.maki.socket.on("disconnect", function() {
+	window.clearInterval(window.maki.interval);
+});
+=======
 	if (window.current_song != update) {
 		// Song change
 		window.current_song = update;
@@ -34,3 +55,4 @@ setInterval(function() {
 	}
 
 }, 8000);
+>>>>>>> 96ce738bccaca0a489bc7d4312b4466d6f9feb9c
